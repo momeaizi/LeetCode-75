9a5db61099549d99c75776f2001a3ca0bd318b3b
Mohamed Taha Meaizi <94363225+momeaizi@users.noreply.github.com>
2024-04-30 15:30:46 +0100

    Time: 74 ms (7.08%), Space: 52.5 MB (64.31%) - LeetHub

---
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.ts
@@ -0,0 +1,14 @@
+function kidsWithCandies(candies: number[], extraCandies: number): boolean[] {
+    const greatest: number = Math.max(...candies);
+    let result: boolean[] = new Array(candies.length);
+
+    for (let i: number = 0; i < candies.length; ++i) {
+        if (candies[i] + extraCandies >= greatest) {
+            result[i] = true;
+        } else {
+            result[i] = false;
+        }
+    }
+
+    return result;
+};
