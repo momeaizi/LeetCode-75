1bd1016d25eed367ca0b3ff52e17e1740a2e4092
Mohamed Taha Meaizi <94363225+momeaizi@users.noreply.github.com>
2024-04-30 09:24:15 +0100

    Time: 4 ms (26.43%), Space: 7.8 MB (19.04%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -1,26 +1,14 @@
 class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
-        string merged = string(word1.length() + word2.length(), ' ');
+    int i = 0;
+    string merged = "";
 
-        int i = word1.length() - 1, j = word2.length() - 1, k = merged.length() - 1;
-
-        while (k >= 0) {
-            if (j >= word1.length()) {
-                merged[k--] = word2[j--];
-            }
-            else if (i >= word2.length()) {
-                merged[k--] = word1[i--];
-            }
-            else {
-                if (j >= 0)
-                    merged[k--] = word2[j--];
-                if (i >= 0)
-                    merged[k--] = word1[i--];
-                cout << merged << endl;
-            }
-        }
-
-        return merged;
+    for (; i < word1.length() && i < word2.length(); ++i) {
+        merged += word1[i];
+        merged += word2[i];
+    }
+    
+    return merged + word1.substr(i) + word2.substr(i);
     }
 };
