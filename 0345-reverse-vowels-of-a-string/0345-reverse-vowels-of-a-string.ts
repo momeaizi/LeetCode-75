25c555615beaa90b13f979f8621be4aa9ad31468
Mohamed Taha Meaizi <94363225+momeaizi@users.noreply.github.com>
2024-05-01 18:36:17 +0100

    Time: 79 ms (38.13%), Space: 55.9 MB (72.39%) - LeetHub

---
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.ts
@@ -0,0 +1,16 @@
+function reverseVowels(s: string): string {
+    let str: string[] = s.split('');
+    let start: number = 0;
+    let end: number = s.length - 1;
+
+    while (start < end) {
+        while (start < end && 'aeiouAEIOU'.includes(s[start]) == false) ++start;
+        while (start < end && 'aeiouAEIOU'.includes(s[end]) == false) --end;
+
+
+        str[start] = s[end];
+        str[end--] = s[start++];
+    }
+
+    return str.join('');
+};
